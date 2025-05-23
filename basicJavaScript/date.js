34e31a25085c69442fe126e22361ef07ab8f89df
Manish-Rana-04 <148924733+Manish-Rana-04@users.noreply.github.com>
2025-05-23 21:21:45 +0000

    Day 1 done

---
+++ basicJavaScript/date.js
@@ -0,0 +1,15 @@
+const currentDate=new Date()
+console.log(currentDate);
+console.log(currentDate.toLocaleDateString());
+console.log (currentDate.toTimeString())
+console.log(currentDate.toDateString());
+console.log(currentDate.getDay());
+
+
+
+
+ 
+const current= Date.now()
+console.log(current.toFixed());
+console.log(current.toString());
+console.log(current.toLocaleString());
---
+++ basicJavaScript/string.js
@@ -0,0 +1,23 @@
+/*
+const name="Rana"
+const age =20
+console.log(`My name is ${name} and my age is ${age}`);
+
+const newName=new String( "Manish Rana")
+console.log(`My name is ${newName}`)
+console.log(newName[4])
+
+*/
+
+/*
+const number= new Number (4008989)
+console.log(number.toFixed(2));
+console.log(number.toPrecision(2));
+console.log(number.toLocaleString('en-IN'))
+
+*/
+const min=15
+const max=27
+console.log(Math.random());
+console.log(Math.floor((Math.random() *(max-min+1))+min));
+
